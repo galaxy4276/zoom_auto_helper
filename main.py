9b2c9a536bda54975a88ec930c505e9905e1aac2
galaxy4276 <galaxyhi4276@gmail.com>
2023-09-22 10:00:21 +0900

    feat: remove unused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pyautogui
 import schedule
-import os
 import pyscreeze
 import PIL
 import time
@@ -12,9 +11,6 @@ ImageGrab.grab = partial(ImageGrab.grab, all_screens=True)
 __PIL_TUPLE_VERSION = tuple(int(x) for x in PIL.__version__.split("."))
 pyscreeze.PIL__version__ = __PIL_TUPLE_VERSION
 
-current_dir = os.getcwd()
-video_on_img = os.path.join(current_dir, 'video_on.png')
-
 
 def click_video():
     pyautogui.moveTo(138, 1286)
@@ -22,6 +18,7 @@ def click_video():
 
 
 if __name__ == '__main__':
+    click_video()
     schedule.every().hour.at(f'50:{get_secs()}').do(click_video)
     schedule.every().hour.at(f'00:{get_secs()}').do(click_video)
 
